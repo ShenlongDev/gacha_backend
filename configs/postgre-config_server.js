52bebff72b1a131f2e94790bc6de228df94ba547
unknown <moon.rider.dev@gmail.com>
2024-12-16 23:21:21 +0900

    update

--- configs/postgre-config_server.js
+++ configs/postgre-config_server.js
@@ -1,11 +1,11 @@
 module.exports = {
   "username": "gacha_admin",
   // "username": "admin",
-  "password": "iEJnvh9JvIAfz36EEJVGSV5yzSFHtWuS",
+  "password": "xuhfkCr117BRlGmwWUcFWVHXeGn8my0R",
   // "password": "4PDrX46rxpFNnr8XP7jhvsK2empxzuxs",
-  "database": "gacha_qcqn",
+  "database": "gacha_3hsd",
   // "database": "gacha_jejs",
-  "host": "dpg-ctg1scbgbbvc73diiis0-a",
+  "host": "dpg-ctg3dn5umphs73eg5uv0-a",
   // "host": "dpg-cja9jgee546c73f70m80-a",
   "dialect": "postgres",
   "logging": false
